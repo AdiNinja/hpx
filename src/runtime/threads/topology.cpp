092b50d97e5b4ecb6b94c875d9b66f7c1cadfeb8
Agustin K-ballo Berge <k@fusionfenix.com>
2017-04-01 16:51:39 -0300

    Add compatibility layer for std::thread

--- src/runtime/threads/topology.cpp
+++ src/runtime/threads/topology.cpp
@@ -6,6 +6,7 @@
 
 #include <hpx/runtime/threads/topology.hpp>
 
+#include <hpx/compat/thread.hpp>
 #include <hpx/error_code.hpp>
 #include <hpx/throw_exception.hpp>
 #include <hpx/runtime/threads/policies/topology.hpp>
@@ -115,7 +116,7 @@ namespace hpx { namespace threads
 #elif defined(HPX_HAVE_HWLOC)
           : num_of_cores_(detail::hwloc_hardware_concurrency())
 #else
-          : num_of_cores_(boost::thread::hardware_concurrency())
+          : num_of_cores_(compat::thread::hardware_concurrency())
 #endif
         {
             if (num_of_cores_ == 0)
